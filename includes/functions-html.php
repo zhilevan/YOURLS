e744fcdcf9cddcf4082827d9372f8f4a1dc5cdd3
ozh <ozh@ozh.org>
2016-03-20 11:10:21 +0100

    Merge fix: yourls_html_logo()

--- includes/functions-html.php
+++ includes/functions-html.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * Display <h1> header and logo
- *
- */
-function yourls_html_logo() {
-	yourls_do_action( 'pre_html_logo' );
-	?>
-	<header role="banner">
-	<h1>
-		<a href="<?php echo yourls_admin_url( 'index.php' ) ?>" title="YOURLS"><span>YOURLS</span>: <span>Y</span>our <span>O</span>wn <span>URL</span> <span>S</span>hortener<br/>
-		<img src="<?php yourls_site_url(); ?>/images/yourls-logo.png" alt="YOURLS" title="YOURLS" border="0" style="border: 0px;" /></a>
-	</h1>
-	</header>
-	<?php
-	yourls_do_action( 'html_logo' );
-}
 
 /**
  * Display HTML head and <body> tag
